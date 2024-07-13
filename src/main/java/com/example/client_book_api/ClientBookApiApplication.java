2c68dd89dcfa6d81775317048da018a0789ec490
Luiz Alberto Furmann <furmannluiz@gmail.com>
2024-07-13 18:51:28 -0300

    first commit

---
+++ src/main/java/com/example/client_book_api/ClientBookApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.client_book_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientBookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientBookApiApplication.class, args);
+	}
+
+}
